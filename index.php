ababc44cfb9106aeecc7d332353ace31cf3fd758
Felipe Borges <lipelsborges@gmail.com>
2026-01-26 17:05:12 -0300

    Novo curso PHP

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gerenciamento de estoque</title>
+    <link rel="shortcut icon" href="images/coruja.png" type="image/x-icon" type="image/png">
+</head>
+<body>
+    <header></header>
+
+    <main></main>
+
+    <footer></footer>
+</body>
+</html>
