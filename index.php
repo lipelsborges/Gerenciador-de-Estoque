b4447bb14891c169b12e20800908975b7624a963
Felipe Borges <lipelsborges@gmail.com>
2026-01-26 17:59:24 -0300

    Montando o corpo

--- index.php
+++ index.php
@@ -1,16 +1,40 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-br" data-bs-theme="dark">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="color-scheme" content="light dark">
     <title>Gerenciamento de estoque</title>
     <link rel="shortcut icon" href="images/coruja.png" type="image/x-icon" type="image/png">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-icons.css">
 </head>
 <body>
-    <header></header>
+    <header class="border-bottom border-primary-subtle bg-body">
+        <div class="container">
+            <div class="row align-items-center py-2 justify-contente-between">
+                <div class="col-4">
+                    <h1 class="fs-4">Flamengo Site</h1>
+                    <h2 class="fs-6 lead">Gerenciamento de estoque</h2>
+                </div>
+                <div class="col-8">
+                    <nav>
+                        <ul class="nav justify-content-end">
+                            <li class="nav-item"><a class="nav-link" href="">Inicio</a></li>
+                            <li class="nav-item"><a class="nav-link" href="">Fornecedores</a></li>
+                            <li class="nav-item"><a class="nav-link" href="">Produtos</a></li>
+                            <li class="nav-item"><a class="nav-link" href="">Lojas</a></li>
+                            <li class="nav-item"><a class="nav-link" href="">Estoque</a></li>
+                        </ul>
+                    </nav>
+                </div>
+    </header>
 
-    <main></main>
+    <main>Conteudo</main>
 
-    <footer></footer>
+    <footer>Rodapé</footer>
+
+    <script src="js/bootstrap.bundle.min.js"></script>
+    
 </body>
 </html>
