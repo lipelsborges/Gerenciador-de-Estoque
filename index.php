383c0ad2f1a47f8e90f77298c05cfeef68effcbd
Felipe Borges <lipelsborges@gmail.com>
2026-01-26 18:37:34 -0300

    inicio quase pronto

--- index.php
+++ index.php
@@ -30,10 +30,54 @@
                 </div>
     </header>
 
-    <main>Conteudo</main>
+    <main class="container my-4">
+        <section class="text-center mb-4 border rounded-3 p-4 border-primary-subtle">
+            <h3>Resumo</h3>
+            <div class="row">
 
-    <footer>Rodapé</footer>
+                <div class="col-6 col-md-4">
+                    <h4><span class="badge text-bg-primary">0</span></h4>
+                    <p><b>Produtos cadastrados</b></p>
 
+                </div>
+                <div class="col-6 col-md-4">
+                    <h4><span class="badge text-bg-primary">0</span></h4>
+                    <p><b>Fornecedores</b></p>
+
+                </div>
+                <div class="col-6 col-md-4">
+                    <h4><span class="badge text-bg-primary">0</span></h4>
+                    <p><b>Lojas ativas</b></p>
+
+                </div>
+                <div class="col-6 col-md-4">
+                    <h4><span class="badge text-bg-primary">0</span></h4>
+                    <p><b>Lojas sem estoque</b></p>
+
+                </div>
+                <div class="col-6 col-md-4">
+                    <h4><span class="badge text-bg-primary">0</span></h4>
+                    <p><b>Estoque < 5</b></p>
+
+                </div>
+                <div class="col-6 col-md-4">
+                    <h4><span class="badge text-bg-primary">0</span></h4>
+                    <p><b>Produtos vencidos ou vencendo em até 30 dias</b></p>
+
+                </div>
+                
+            </div>
+        </section>
+        <section class="text-center mb-4 border rounded-3 p-4 border-primary-subtle">
+            <h3>Relatórios</h3>
+            <a href="/produtos-por-loja" class="btn btn-lg btn-outline-primary my-1">Produtos por Loja</a>
+            <a href="" class="btn btn-lg btn-outline-primary my-1">Produtos por Fornecedor</a>
+            <a href="" class="btn btn-lg btn-outline-primary my-1">Estoque por Produto</a>
+            <a href="" class="btn btn-lg btn-outline-primary my-1">Estoque Baixo</a>
+        </section>
+    </main>
+
+    
     <script src="js/bootstrap.bundle.min.js"></script>
     
 </body>
