7489d333c3309f71e298d4dc01a01aba5db91495
Felipe Borges <lipelsborges@gmail.com>
2026-02-02 16:50:17 -0300

    Criando o html da rota listar.php

--- fornecedores/listar.php
+++ fornecedores/listar.php
@@ -8,7 +8,33 @@ require_once BASE_PATH . '/includes/cabecalho.php';
 ?>
 
 <section class="text-center mb-4 border rounded-3 p-4 border-primary-subtle">
-    <h3><i class="bi bi-journal-check fs-4"></i> Fornecedores</h3>
+    <h3><i class="bi bi-people-fill"></i> Fornecedores</h3>
+    <p>
+        <a  class="btn btn-primary" href="inserir.php">
+           <i class="bi bi-plus-circle"></i> Adicionar novo fornecedor
+        </a>
+    </p>
+
+    <div class="table-responsive">
+        <table class="table table-hover caption-top">
+            <caption>Quantidade de registros: 0</caption>
+            <thead class="align-middle table-light">
+                <tr>
+                    <th >ID</th>
+                    <th >Nome</th>
+                    <th colspan="2">Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td >Id do fornecedor</td>
+                    <td >Nome do fornecedor</td>
+                    <td ><a href="" class="btn btn-warning btn-sm"><i class="bi bi-pencil-square"></i> Editar</a></td>
+                    <td ><a href="" class="btn btn-danger btn-sm"><i class="bi bi-trash"></i> Excluir</a></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </section>
 
 <?php require_once BASE_PATH . '/includes/rodape.php'; ?>
