bcf770d9a116ba444949a51452097c6cf01aa3c8
Felipe Borges <lipelsborges@gmail.com>
2026-02-02 18:41:05 -0300

    Modificações

--- includes/cabecalho.php
+++ includes/cabecalho.php
@@ -10,7 +10,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="color-scheme" content="light dark">
-    <title>Gerenciamento de estoque</title>
+    <title><?=$titulo ?? ''?> Fly By Day - Gerenciamento de estoque</title>
     <link rel="shortcut icon" href="<?= BASE_URL ?>/images/coruja.png" type="image/x-icon" type="image/png">
     <link rel="stylesheet" href="<?= BASE_URL ?>/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
@@ -21,7 +21,7 @@
         <div class="container">
             <div class="row align-items-center py-2 justify-contente-between">
                 <div class="col-4">
-                    <h1 class="fs-4"><a class="text-decoration-none" href="<?= BASE_URL ?>/index.php">Fly by Day</a></h1>
+                    <h1 class="fs-4"><a class="text-decoration-none" href="<?= BASE_URL ?>/index.php">Fly By Day</a></h1>
                     <h2 class="fs-6 lead">Gerenciamento de estoque</h2>
                 </div>
                 <div class="col-8">
