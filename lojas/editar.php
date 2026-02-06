a7d8369310cbc367e6744316a727cbd8f635ff83
Felipe Borges <lipelsborges@gmail.com>
2026-02-06 13:47:45 -0300

    Pagina Loja criada

---
+++ lojas/editar.php
@@ -0,0 +1,25 @@
+<?php 
+
+require_once __DIR__ . '/../config.php';
+$titulo = "Editar Fornecedor |";
+require_once BASE_PATH . '/includes/cabecalho.php'; 
+
+
+
+?>
+
+<section class="mb-4 border rounded-3 p-4 border-primary-subtle">
+    <h3 class="text-center"><i class="bi bi-pencil-fill"></i> Editar Loja</h3>
+
+    <form action="" method="post" class="w-75 mx-auto">
+        <input type="hidden" name="id" value="ID da loja...">
+        <div class="form-group">
+            <label for="nome" class="form-label">Nome: </label>
+            <input type="text" class="form-control" id="nome" name="nome" value="Nome da loja...">
+        </div>
+        <button class="btn btn-warning my-4" type="submit"><i class="bi bi-arrow-clockwise"></i>  Salvar Alterações</button>
+    </form>
+
+</section>
+
+<?php require_once BASE_PATH . '/includes/rodape.php'; ?>
---
+++ lojas/inserir.php
@@ -0,0 +1,25 @@
+<?php 
+
+require_once __DIR__ . '/../config.php';
+$titulo = "Adicionar Loja |";
+require_once BASE_PATH . '/includes/cabecalho.php'; 
+
+
+
+?>
+
+<section class="mb-4 border rounded-3 p-4 border-primary-subtle">
+    <h3 class="text-center"><i class="bi bi-plus-circle-fill"></i> Adicionar Loja</h3>
+
+    <form action="" method="post" class="w-75 mx-auto">
+        <div class="form-group">
+            <label for="nome" class="form-label">Nome: </label>
+            <input type="text" class="form-control" id="nome" name="nome">
+        </div>
+        <a href="listar.php" class="btn btn-secondary my-4"><i class="bi bi-arrow-left-circle"></i> Voltar</a>
+        <button class="btn btn-success my-4" type="submit"><i class="bi bi-check-circle"></i>  Salvar</button>
+    </form>
+
+</section>
+
+<?php require_once BASE_PATH . '/includes/rodape.php'; ?>
