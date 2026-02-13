7acac80e0d98d6d885cea9963d945c2223e68ac1
Felipe Borges <lipelsborges@gmail.com>
2026-02-13 11:27:24 -0300

    Capturando os Dados  preenchidos.

--- usuarios/inserir.php
+++ usuarios/inserir.php
@@ -1,7 +1,24 @@
 <?php 
 
 require_once __DIR__ . '/../config.php';
+
+    $erro = null;
+    $msg_success = null;
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $senhaForm = $_POST['senha'];
+
+        if(empty($nome) || empty($email) || empty($senhaForm)){
+            $erro = "Preencha todos os campos!";
+        } else {
+            $msg_success = "Usuário adicionado!";
+        }
+    }
+
 $titulo = "Adicionar Usuário |";
+
 require_once BASE_PATH . '/includes/cabecalho.php'; 
 
 
@@ -15,18 +32,26 @@ require_once BASE_PATH . '/includes/cabecalho.php';
 
         <div class="form-group">
             <label for="nome" class="form-label">Nome: </label>
-            <input type="text" class="form-control" id="nome" name="nome" required>
+            <input type="text" class="form-control" id="nome" name="nome" >
         </div>
 
         <div class="form-group">
             <label for="email" class="form-label">Email: </label>
-            <input type="email" class="form-control" id="email" name="email" required>
+            <input type="email" class="form-control" id="email" name="email" >
         </div>
 
         <div class="form-group">
             <label for="senha" class="form-label">Senha: </label>
-            <input type="password" class="form-control" id="senha" name="senha" required>
+            <input type="password" class="form-control" id="senha" name="senha" >
         </div>
+
+    <?php if($erro):  ?>
+        <br><p class="alert alert-danger text-center"><?=$erro ?></p>
+    <?php endif; ?>
+
+    <?php if($msg_success): ?>
+        <br><p class="alert alert-success text-center"><?=$msg_success?></p>
+    <?php endif; ?>
 
         <a href="listar.php" class="btn btn-secondary my-4"><i class="bi bi-arrow-left-circle"></i> Voltar</a>
         <button class="btn btn-success my-4" type="submit"><i class="bi bi-check-circle"></i>  Salvar</button>
