470781a6474b721a399a83c74f2bc0bf645089f0
Felipe Borges <lipelsborges@gmail.com>
2026-02-18 15:40:34 -0300

    utilizando a função verificarsenha e atualizarusuario

--- usuarios/editar.php
+++ usuarios/editar.php
@@ -1,33 +1,74 @@
-<?php 
+<?php
 
 require_once __DIR__ . '/../config.php';
 require_once BASE_PATH . "/src/usuario_crud.php";
-
 require_once BASE_PATH . '/src/utils.php';
+
 
 $id = sanitizar($_GET['id'], 'inteiro');
 $erro = null;
+$sucess = null;
 
-if(!$id){
+if (!$id) {
 
     header("location:listar.php");
     exit;
-
 }
 
 try {
-    
-    $usuario = buscarUsuarioPorId( $conexao ,$id);
-    if(!$usuario) $erro = "Usuário não encontrado!";
+
+    $usuario = buscarUsuarioPorId($conexao, $id);
+    if (!$usuario) $erro = "Usuário não encontrado!";
     //dump($usuario);
 
 } catch (Throwable $error) {
-    $erro = "Erro ao buscar usuário <br>" .$error->getMessage();
+    $erro = "Erro ao buscar usuário <br>" . $error->getMessage();
 }
 
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $nome = sanitizar($_POST['nome'], 'nome');
+    $email = sanitizar($_POST['email'], 'email');
+    $senhaForm = $_POST['senha'];
+
+    if (empty($nome) || empty($email)) {
+
+        $erro = "Nome e E-mail são obrigatórios.";
+    } else {
+
+        try {
+
+            //A Pergunta: O campo de senha no formulário ($senhaForm) veio vazio?
+            //Se SIM (?): O usuário não quer mudar a senha. Então, $senhaVerificada recebe o que já está no banco ($usuario['senha']).
+            //Se NÃO (:): O usuário digitou algo. Então, o código chama a função verificarSenha() para tratar esse novo dado antes de atribuí-lo à variável.
+
+            $senhaVerificada = empty($senhaForm) ? $usuario['senha'] : verificarSenha($senhaForm, $usuario['senha']);
+
+            atualizarUsuario($conexao, $id, $nome, $email, $senhaVerificada);
+
+            $sucess = "Editado com sucesso! ";
+
+            header("location:listar.php");
+            
+            exit;
+
+        } catch (Throwable $error) {
+            if ($error->getCode() === '23000') {
+
+                $erro = "E-mail já cadastrado. Por favor, use outro e-mail.";
+            } else {
+
+                $erro = "Erro ao atualizar usuário: <br>" . $error->getMessage();
+            }
+        }
+    }
+}
+
+
+
 $titulo = "Editar Usuário |";
-require_once BASE_PATH . '/includes/cabecalho.php'; 
+require_once BASE_PATH . '/includes/cabecalho.php';
 
 
 
@@ -36,9 +77,11 @@ require_once BASE_PATH . '/includes/cabecalho.php';
 <section class="mb-4 border rounded-3 p-4 border-primary-subtle">
     <h3 class="text-center"><i class="bi bi-pencil-fill"></i> Editar Usuário</h3>
 
-    <?php if($erro):  ?>
-        <br><p class="alert alert-danger text-center"><?=$erro ?></p>
+    <?php if ($erro):  ?>
+        <br>
+        <p class="alert alert-danger text-center"><?= $erro ?></p>
     <?php endif; ?>
+
 
 
 
@@ -48,19 +91,19 @@ require_once BASE_PATH . '/includes/cabecalho.php';
 
         <div class="form-group">
             <label for="nome" class="form-label">Nome: </label>
-            <input type="text" class="form-control" id="nome" name="nome" value="<?= $usuario['nome'] ?? '' ?>">
+            <input required type="text" class="form-control" id="nome" name="nome" value="<?= $usuario['nome'] ?? '' ?>">
         </div>
 
         <div class="form-group">
             <label for="email" class="form-label">Email: </label>
-            <input type="email" class="form-control" id="email" name="email" value="<?= $usuario['email'] ?? '' ?>">
+            <input required type="email" class="form-control" id="email" name="email" value="<?= $usuario['email'] ?? '' ?>">
         </div>
 
         <div class="form-group">
             <label for="senha" class="form-label">Senha: </label>
             <input type="password" class="form-control" id="senha" name="senha" placeholder="Preencha apenas se for alterar a Senha">
         </div>
-        <button class="btn btn-warning my-4" type="submit"><i class="bi bi-arrow-clockwise"></i>  Salvar Alterações</button>
+        <button class="btn btn-warning my-4" type="submit"><i class="bi bi-arrow-clockwise"></i> Salvar Alterações</button>
     </form>
 
 </section>
