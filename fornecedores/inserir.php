2f29c3c618df69a411c7b4c2e590457a4472c596
Felipe Borges <lipelsborges@gmail.com>
2026-02-19 09:32:54 -0300

    Função inserir Fornecedores criada

--- fornecedores/inserir.php
+++ fornecedores/inserir.php
@@ -3,6 +3,16 @@
 require_once __DIR__ . '/../config.php';
 $titulo = "Adicionar Fornecedor |";
 require_once BASE_PATH . '/includes/cabecalho.php'; 
+require_once BASE_PATH . '/src/fornecedor_crud.php';
+
+$erro = null;
+
+try {
+
+  
+} catch (Throwable $error) {
+    
+}
 
 
 
