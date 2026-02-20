0c34789cb18fc2cac1aec0f8560f0422839b35ee
Felipe Borges <lipelsborges@gmail.com>
2026-02-20 10:33:31 -0300

    implementando required no input

--- login.php
+++ login.php
@@ -68,12 +68,12 @@ include_once BASE_PATH . '/includes/cabecalho.php';
 
         <div class="mb-3">
             <label for="email" class="form-label">E-mail</label>
-            <input type="email" class="form-control" id="email" name="email" >
+            <input required type="email" class="form-control" id="email" name="email" >
         </div>
 
         <div class="mb-3">
             <label for="senha" class="form-label">Senha</label>
-            <input type="password" class="form-control" id="senha" name="senha" >
+            <input required type="password" class="form-control" id="senha" name="senha" >
         </div>
         <button type="submit" class="btn btn-primary">Entrar</button>
 
