99f8ba6cd4e92cafb0d4cd7b5c914ea9626d9a22
Felipe Borges <lipelsborges@gmail.com>
2026-02-23 12:59:11 -0300

    ajuste

--- src/fornecedor_crud.php
+++ src/fornecedor_crud.php
@@ -1,7 +1,7 @@
 <?php 
 
 function buscarFornecedor(PDO $conexao): array {
-    $sql = "SELECT id, nome FROM fornecedores ORDER BY id";
+    $sql = "SELECT * FROM fornecedores ORDER BY id";
 
     $query= $conexao->prepare($sql);
     $query->execute();
