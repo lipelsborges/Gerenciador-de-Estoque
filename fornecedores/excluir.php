7c2924ae1e210a3a006d13345459787cb5df735b
Felipe Borges <lipelsborges@gmail.com>
2026-02-23 11:16:17 -0300

    excluir fornecedor funcionando.

--- fornecedores/excluir.php
+++ fornecedores/excluir.php
@@ -11,6 +11,32 @@ exigirLogin();
 $id = sanitizar($_GET['id'], 'inteiro');
 $erro = null;
 
+try {
+
+   $fornecedor =  buscarFornecedorPorId($conexao, $id);
+    if(!$fornecedor) $erro = 'Fornecedor não encontrado!';
+
+    
+} catch (Throwable $error) {
+
+    $erro = "Erro ao Buscar Fornecedor: <br>". $error->getMessage();
+    
+}
+
+if(isset($_GET['confirmar-exclusao']) && !$erro)
+    try {
+
+        excluirFornecedor($conexao, $id);
+        header("location:listar.php");
+        exit;
+
+    } catch (\Throwable $error) {
+
+        $erro = "Erro ao Buscar Fornecedor: <br>". $error->getMessage();
+        
+    }
+
+
 
 
 
@@ -18,11 +44,12 @@ $erro = null;
 
 <section class="mb-4 border rounded-3 p-4 border-primary-subtle">
     <h3 class="text-center"><i class="bi bi-trash3-fill"></i> Excluir Fornecedor</h3>
+    
 
     <div class="alert alert-danger w-50 text-center mx-auto">
-        <p>Tem certeza que deseja excluir o fornecedor <b>Nome do fornecedor</b>?</p>
+        <p>Tem certeza que deseja excluir o fornecedor <b><?= $fornecedor['nome'] ?></b>?</p>
         <a href="listar.php" class="btn btn-secondary"><i class="bi bi-x-circle"></i> Não</a>
-        <a href="" class="btn btn-danger"><i class="bi bi-check-circle"></i> Sim</a>
+        <a href="?id=<?= $fornecedor['id'] ?>&confirmar-exclusao" class="btn btn-danger"><i class="bi bi-check-circle"></i> Sim</a>
     </div>
 
 </section>
