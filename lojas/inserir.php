2a845c9252d9bfd2756dd93584cd8c96316ac602
Felipe Borges <lipelsborges@gmail.com>
2026-02-24 14:09:36 -0300

    Utilizando a função inserirLojas

--- lojas/inserir.php
+++ lojas/inserir.php
@@ -3,15 +3,47 @@
 require_once __DIR__ . '/../config.php';
 $titulo = "Adicionar Loja |";
 require_once BASE_PATH . '/includes/cabecalho.php';
+require_once BASE_PATH . '/src/lojas_crud.php';
+require_once BASE_PATH . '/src/utils.php';
 
+
+$erro = null;
 exigirLogin();
 
+if($_SERVER['REQUEST_METHOD']=== 'POST'){
+    $nome = sanitizar($_POST['nome'], 'texto');
+
+    if(empty($nome)){
+        
+        $erro = 'Campo nome obrigatório!';
+        
+
+    } else{
+
+        try {
+            inserirLoja($conexao, $nome);
+            header('location:listar.php');
+            exit;
+
+        } catch (Throwable $error) {
+
+            $erro = "Erro ao inserir a Loja". $error->getMessage();
+
+        }
+    }
+}
+        
 
 
 ?>
 
 <section class="mb-4 border rounded-3 p-4 border-primary-subtle">
     <h3 class="text-center"><i class="bi bi-plus-circle-fill"></i> Adicionar Loja</h3>
+
+    <?php if($erro):  ?>
+        <br><p class="alert alert-danger text-center "><strong><?=$erro ?></strong></p>
+    <?php endif; ?>
+
 
     <form action="" method="post" class="w-75 mx-auto">
         <div class="form-group">
