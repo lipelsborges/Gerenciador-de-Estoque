eda63191bccd3abf02b03db6642041af788e04ac
Felipe Borges <lipelsborges@gmail.com>
2026-02-26 14:21:04 -0300

    ajuste

--- src/banco.php
+++ src/banco.php
@@ -10,7 +10,7 @@
 
     try {
 
-        $conexao = new PDO("mysql:host=$servidor; dbname=$banco; charset=utf8", $usuario, $senha);
+        $conexao = new PDO("mysql:host=$servidor; dbname=$banco; charset=utf8mb4", $usuario, $senha);
         
         
         $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
