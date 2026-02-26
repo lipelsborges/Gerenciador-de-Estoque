88a4fbcdda83492156f422a9cc1301aa6622a3ac
Felipe Borges <lipelsborges@gmail.com>
2026-02-26 14:23:31 -0300

    buscar funcionando

--- produtos/listar.php
+++ produtos/listar.php
@@ -9,21 +9,21 @@ require_once BASE_PATH .'/src/utils.php';
 
 exigirLogin();
 
+$termoBusca = sanitizar($_GET['buscar'] ?? '', 'texto') ?? null;
+
 $erro = null;
+$erroValidacaoBusca = null;
 $produtos = [];
 
-try {
-
-    $produtos = buscarProduto($conexao);
-
-    
-} catch (Throwable $error) {
-
-    $erro = "Erro ao buscar o produto <br>" . $error->getMessage();
-    
+if(isset($_GET['buscar']) && empty($termoBusca)){
+    $erroValidacaoBusca = "O termo de busca não pode ser vazio.";
 }
 
-
+try {
+    $produtos = buscarProduto($conexao, $termoBusca);
+} catch (Throwable $error) {
+    $erro = "Erro ao buscar o produto <br>" . $error->getMessage();
+}
 
 ?>
 
@@ -35,10 +35,17 @@ try {
         </a>
     </p>
 
+    <?php if($erro):  ?>
+        <p class="alert alert-danger text-center"><?=$erro ?></p>
+    <?php endif; ?>
+    <?php if($erroValidacaoBusca):  ?>
+        <p class="alert alert-danger text-center"><?=$erroValidacaoBusca ?></p>
+    <?php endif; ?>
+
     <form action="" method="get" class="mx-auto my-4">
         <div class="row  g-2 justify-content-center mb-3">
             <div class="col-auto">
-                <input  type="search" name="search" class="form-control" placeholder="Buscar produto...">
+                <input  required id = "buscar" type="buscar" name="buscar" class="form-control" placeholder="Buscar produto..." value="<?=$termoBusca?>">
             </div>
             <div class="col-auto">
                 <button type="submit" class="btn btn-success w-100">
@@ -48,9 +55,25 @@ try {
         </div>
     </form>
 
-    <?php if($erro):  ?>
-        <p class="alert alert-danger text-center"><?=$erro ?></p>
-    <?php endif; ?>
+        <?php 
+            $mensagem = "";
+
+            if($termoBusca !== ""):
+                if(!empty($produtos)):
+                    $mensagem = "<p class='text-muted'>Resultados para <b class = 'bg-info-subtle rounded p-1'>$termoBusca</b></p>";  
+                else:
+                    $mensagem = "<p class = 'text-danger'>Nenhum produto foi encontrado</p>";
+                endif;   
+
+                echo $mensagem;
+        ?>
+            <a href="listar.php" class="btn btn-sm btn-outline-secondary">&times;Limpar Busca</a>
+            
+        <?php endif; ?>
+      
+        
+
+        
 
     <div class="table-responsive">
         <table class="table table-hover caption-top">
@@ -70,9 +93,9 @@ try {
                 <tr>
                     <td ><?= $produto['nome'] ?></td>
                     <td ><?= $produto['descricao'] ?></td>
-                    <td ><?= $produto['Nome_Fornecedor'] ?></td>
-                    <td ><?= $produto['preco'] ?></td>
-                    <td ><?= $produto['data_validade'] ?></td>
+                    <td ><?= $produto['fornecedor'] ?></td>
+                    <td ><?= formatarPreco($produto['preco']) ?></td>
+                    <td ><?= formatarData($produto['data_validade']) ?></td>
                     <td ><a href="editar.php" class="btn btn-warning btn-sm"><i class="bi bi-pencil-square"></i> Editar</a></td>
                     <td ><a href="excluir.php" class="btn btn-danger btn-sm"><i class="bi bi-trash"></i> Excluir</a></td>
                 </tr>
