719e25e9c862af058479894504f6ecb95ee65fe2
Felipe Borges <lipelsborges@gmail.com>
2026-02-26 14:21:42 -0300

    Colocando formulario de buscar para funcionar

--- src/produtos_crud.php
+++ src/produtos_crud.php
@@ -1,21 +1,38 @@
 <?php
 
 
-function buscarProduto(PDO $conexao): array {
+function buscarProduto(PDO $conexao, string $busca = ''): array {
 
     $sql = 'SELECT 
-	        pr.nome,
+            pr.nome,
             pr.descricao,
-            f.nome "Nome_Fornecedor",
+            f.nome "fornecedor",
             pr.preco,
             dpr.data_validade
         FROM sys.produtos pr
-        INNER JOIN fornecedores f on f.id = pr.fornecedor_id
-        INNER JOIN detalhes_produto dpr on dpr.produto_id = pr.id
-        ORDER BY nome ASC';
+        LEFT JOIN fornecedores f on f.id = pr.fornecedor_id
+        LEFT JOIN detalhes_produto dpr on dpr.produto_id = pr.id';
+    
+    $parametros = [];
 
+    if(!empty($busca)){
+        
+        $buscaLimpa = html_entity_decode($busca, ENT_QUOTES | ENT_HTML5, 'UTF-8');
+        
+        $sql .= " WHERE pr.nome COLLATE utf8mb4_general_ci LIKE :busca1 
+                  OR pr.descricao COLLATE utf8mb4_general_ci LIKE :busca2";
+        
+        $valor = "%$buscaLimpa%";
+        $parametros[':busca1'] = $valor;
+        $parametros[':busca2'] = $valor;
+    }
+
+   
+    $sql .= " ORDER BY pr.nome ASC";
+    
     $query = $conexao->prepare($sql);
-    $query->execute();
+    $query->execute($parametros);
+    
     return $query->fetchAll(PDO::FETCH_ASSOC);
-
 }
+
