38ea230b7e464a1a83832282f85b622b85853ccb
Felipe Borges <lipelsborges@gmail.com>
2026-02-27 13:55:24 -0300

    Criando a função inserirProduto e inserirDetalheDoProduto

--- src/produtos_crud.php
+++ src/produtos_crud.php
@@ -36,3 +36,30 @@ function buscarProduto(PDO $conexao, string $busca = ''): array {
     return $query->fetchAll(PDO::FETCH_ASSOC);
 }
 
+function inserirProduto(PDO $conexao, array $produto): void{
+
+    $sql = "INSERT INTO produtos(nome, descricao, preco, quantidade, fornecedor_id)
+            VALUES(:nome, :descricao, :preco, :quantidade, :fornecedor_id)";
+    $query = $conexao->prepare($sql);
+    $query->bindValue(":nome", $produto['nome'], PDO::PARAM_STR);
+    $query->bindValue(":descricao", $produto['descricao']
+                      , is_null($produto['descricao'] ? PDO::PARAM_NULL : PDO::PARAM_STR) );
+    $query->bindValue(":preco", $produto['preco'], PDO::PARAM_STR);
+    $query->bindValue(":quantidade", $produto['quantidade'], PDO::PARAM_INT);
+    $query->bindValue(":fornecedor_id", $produto['fornecedor'], PDO::PARAM_INT);
+
+    $query->execute();
+}
+
+function inserirDetalhesDoProduto(PDO $conexao, array $detalhes):void {
+
+    $sql = "INSERT INTO detalhes_produtos(produto_id, peso, dimensoes, codigo_barras, data_validade)
+            VALUES(:produto_id, :peso, :dimensoes, :codigo_barras, :data_validade)";
+    $query= $conexao->prepare($sql);
+    $query->bindValue(":produto_id", $detalhes['produto_id'], PDO::PARAM_STR);
+    $query->bindValue(":peso", $detalhes['peso'], is_null($detalhes['peso']) ? PDO::PARAM_NULL : PDO::PARAM_STR);
+    $query->bindValue(":dimensoes", $detalhes['dimensoes'], is_null($detalhes['dimensoes']) ? PDO::PARAM_NULL : PDO::PARAM_STR);
+    $query->bindValue(":codigo_barras", $detalhes['codigo_barras'], is_null($detalhes['codigo_barras']) ? PDO::PARAM_NULL : PDO::PARAM_STR);
+    $query->bindValue(":data_validade", $detalhes['data_validade'], is_null($detalhes['data_validade']) ? PDO::PARAM_NULL : PDO::PARAM_STR);
+    $query->execute();
+}
