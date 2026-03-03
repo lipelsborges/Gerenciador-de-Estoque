7d85b4e0b58ac1939a75686f509624349fad6fbf
Felipe Borges <lipelsborges@gmail.com>
2026-03-03 10:18:16 -0300

    editar produto funcinando

--- produtos/editar.php
+++ produtos/editar.php
@@ -19,16 +19,17 @@ if(!$id){
 
 try {
 
-    $fornecedor = buscarFornecedorPorId($conexao, $id);
-    $produtos = buscarProdutoPorId($conexao, $id);
-    if (!$produtos) $erro[]= "Produto não encontrado!";
-    if (!$fornecedor) $erro[]= "Fornecedor não encontrado!";
+    
+    $produto = buscarProdutoPorId($conexao, $id);
+    if (!$produto) $erro[]= "Produto não encontrado!";
+    
 
 } catch (Throwable $error) {
 
 $erro[] = "Erro ao buscar produto<br>". $error->getMessage();
     
 }
+
 
 
 
@@ -49,39 +50,72 @@ $erro[] = "Erro ao buscar produto<br>". $error->getMessage();
     <?php endif; ?>
 
     <form action="" method="post" class="w-75 mx-auto">
-        <input type="hidden" name="id" value="ID do produto...">
+
+        <legend>Produto</legend>
+        
+        <input type="hidden" name="detalhe_id" value="<?=$produto['detalhe_id'] ?? '' ?>">
+        <input type="hidden" name="id" value="<?=$produto['produto_id'] ?? '' ?>">
+
         <div class="form-group">
+
             <label for="nome" class="form-label">Nome: </label>
-            <input type="text" class="form-control" id="nome" name="nome" value="<?= $produtos['nome'] ?>">
+            <input type="text" class="form-control" id="nome" name="nome" value="<?= $produto['nome'] ?>">
 
             <label for="descricao" class="form-label">Descrição: </label>
-            <input type="text" class="form-control" id="descricao" name="descricao" value="<?= $produtos['descricao'] ?>">
+            <input type="text" class="form-control" id="descricao" name="descricao" value="<?= $produto['descricao'] ?>">
 
             <label for="fornecedor" class="form-label">Fornecedor: </label>
             <select class="form-select" id="fornecedor" name="fornecedor">
 
-                <option value="<?= $fornecedor['nome'] ?>"></option>
+                <option value=""></option>
+
+                <?php  
+
+                    $fornecedores = buscarFornecedor($conexao);
+                    foreach($fornecedores as $fornecedor):
+                        
+                    $selecionado = ($fornecedor['id'] === $produto['fornecedor_id']) ? 'selected' : '';
+                ?>
+                <option value="<?= $fornecedor['id'] ?>" <?= $selecionado ?>><?= $fornecedor['nome'] ?></option>
+                <?php  
+                
+                    endforeach;
+
+                ?>
 
 
             </select>
+
             <label for="preco" class="form-label">Preço: </label>
-            <input type="number" step="0.01" class="form-control" id="preco" name="preco" value="0.00">
+            <input type="number" step="0.01" class="form-control" id="preco" name="preco" value="<?= $produto['preco'] ?>">
+
 
             <label for="quantidade" class="form-label">Quantidade: </label>
-            <input type="number" class="form-control" id="quantidade" name="quantidade" min="0" value="0">
+            <input type="number" class="form-control" id="quantidade" name="quantidade" min="0" value="<?= $produto['quantidade']?>">
+
 
             <h4 class="mt-4">Detalhe do Produto</h4>
+
+
             <label for="detalhes" class="form-label">Peso(kg): </label>
-            <input type="number" step="0.01" class="form-control" id="peso" name="peso" value="0.00">  
+            <input type="number" step="0.01" class="form-control" id="peso" name="peso" value="<?= $produto['peso'] ?? '' ?>"> 
+
+
 
             <label for="detalhes" class="form-label mt-2">Dimensões (LxAxP): </label>
-            <input type="text" class="form-control" id="dimensoes" name="dimensoes" value="0x0x0">
+            <input type="text" class="form-control" id="dimensoes" name="dimensoes" value="<?= $produto['dimensoes'] ?? ''?>">
+
+
 
             <label for="codigo_barras" class="form-label mt-2">Código de Barras: </label>
-            <input type="text" class="form-control" id="codigo_barras" name="codigo_barras" value="0000000000000">
+            <input type="text" class="form-control" id="codigo_barras" name="codigo_barras" value="<?= $produto['codigo_barras'] ?? ''?>">
+
+
 
             <label for="data_validade" class="form-label">Data de Validade: </label>
-            <input type="date" class="form-control" id="data_validade" name="data_validade" value="2025-12-31">
+            <input type="date" class="form-control" id="data_validade" name="data_validade" value="<?= $produto['data_validade'] ?? '' ?>">
+            
+
             
         </div>
         <a href="<?= BASE_URL ?>/produtos/listar.php" class="btn btn-secondary my-4"><i class="bi bi-arrow-left"></i> Voltar</a>
