b661a342ef7f3f62f4fea3467cbfcfb2f9246ac8
Felipe Borges <lipelsborges@gmail.com>
2026-03-04 09:24:05 -0300

    Criando a função atualizarDetalhesDoProduto

--- src/produtos_crud.php
+++ src/produtos_crud.php
@@ -1,7 +1,8 @@
 <?php
 
 
-function buscarProduto(PDO $conexao, string $busca = ''): array {
+function buscarProduto(PDO $conexao, string $busca = ''): array
+{
 
     $sql = 'SELECT 
             pr.id,
@@ -13,52 +14,57 @@ function buscarProduto(PDO $conexao, string $busca = ''): array {
         FROM sys.produtos pr
         LEFT JOIN fornecedores f on f.id = pr.fornecedor_id
         LEFT JOIN detalhes_produto dpr on dpr.produto_id = pr.id';
-    
+
     $parametros = [];
 
-    if(!empty($busca)){
-        
+    if (!empty($busca)) {
+
         $buscaLimpa = html_entity_decode($busca, ENT_QUOTES | ENT_HTML5, 'UTF-8');
-        
+
         $sql .= " WHERE pr.nome COLLATE utf8mb4_general_ci LIKE :busca1 
                   OR pr.descricao COLLATE utf8mb4_general_ci LIKE :busca2";
-        
+
         $valor = "%$buscaLimpa%";
         $parametros[':busca1'] = $valor;
         $parametros[':busca2'] = $valor;
     }
 
-   
+
     $sql .= " ORDER BY pr.nome ASC";
-    
+
     $query = $conexao->prepare($sql);
     $query->execute($parametros);
-    
+
     return $query->fetchAll(PDO::FETCH_ASSOC);
 }
 
-function inserirProduto(PDO $conexao, array $produto): int{
+function inserirProduto(PDO $conexao, array $produto): int
+{
 
     $sql = "INSERT INTO produtos(nome, descricao, preco, quantidade, fornecedor_id)
             VALUES(:nome, :descricao, :preco, :quantidade, :fornecedor_id)";
     $query = $conexao->prepare($sql);
     $query->bindValue(":nome", $produto['nome'], PDO::PARAM_STR);
-    $query->bindValue(":descricao", $produto['descricao']
-                      , is_null($produto['descricao']) ? PDO::PARAM_NULL : PDO::PARAM_STR);
+    $query->bindValue(
+        ":descricao",
+        $produto['descricao'],
+        is_null($produto['descricao']) ? PDO::PARAM_NULL : PDO::PARAM_STR
+    );
     $query->bindValue(":preco", $produto['preco'], PDO::PARAM_STR);
     $query->bindValue(":quantidade", $produto['quantidade'], PDO::PARAM_INT);
     $query->bindValue(":fornecedor_id", $produto['fornecedor_id'], PDO::PARAM_INT);
 
     $query->execute();
     // Retornando o ID do produto recém-inserido
-   return (int) $conexao->lastInsertId();
+    return (int) $conexao->lastInsertId();
 }
 
-function inserirDetalhesDoProduto(PDO $conexao, array $detalhes):void {
+function inserirDetalhesDoProduto(PDO $conexao, array $detalhes): void
+{
 
     $sql = "INSERT INTO detalhes_produto(produto_id, peso, dimensoes, codigo_barras, data_validade)
             VALUES(:produto_id, :peso, :dimensoes, :codigo_barras, :data_validade)";
-    $query= $conexao->prepare($sql);
+    $query = $conexao->prepare($sql);
     $query->bindValue(":produto_id", $detalhes['produto_id'], PDO::PARAM_STR);
 
     $query->bindValue(":peso", $detalhes['peso'], is_null($detalhes['peso']) ? PDO::PARAM_NULL : PDO::PARAM_STR);
@@ -71,7 +77,8 @@ function inserirDetalhesDoProduto(PDO $conexao, array $detalhes):void {
     $query->execute();
 }
 
-function buscarProdutoPorId(PDO $conexao, int $id): ?array {
+function buscarProdutoPorId(PDO $conexao, int $id): ?array
+{
 
     $sql = 'SELECT 
             pr.id,
@@ -89,10 +96,55 @@ function buscarProdutoPorId(PDO $conexao, int $id): ?array {
         LEFT JOIN fornecedores f on f.id = pr.fornecedor_id
         LEFT JOIN detalhes_produto dpr on dpr.produto_id = pr.id
         WHERE pr.id = :id';
-    
+
     $query = $conexao->prepare($sql);
     $query->bindValue(":id", $id, PDO::PARAM_INT);
     $query->execute();
-    
+
     return $query->fetch(PDO::FETCH_ASSOC) ?: null;
+}
+
+function atualizarProduto(PDO $conexao, array $produtoAtualizado): void
+{
+
+    $sql = "UPDATE produtos SET
+                nome = :nome,
+                descricao = :descricao,
+                preco = :preco,
+                quantidade = :quantidade,
+                fornecedor_id = :fornecedor_id
+            WHERE id = :id";
+
+    $query = $conexao->prepare($sql);
+    $query->bindValue(":id", $produtoAtualizado['id'], PDO::PARAM_INT);
+    $query->bindValue(":nome", $produtoAtualizado['nome'], PDO::PARAM_STR);
+    $query->bindValue(
+        ":descricao",
+        $produtoAtualizado['descricao'],
+        is_null($produtoAtualizado['descricao']) ? PDO::PARAM_NULL : PDO::PARAM_STR
+    );
+    $query->bindValue(":preco", $produtoAtualizado['preco'], PDO::PARAM_STR);
+    $query->bindValue(":quantidade", $produtoAtualizado['quantidade'], PDO::PARAM_INT);
+    $query->bindValue(":fornecedor_id", $produtoAtualizado['fornecedor_id'], PDO::PARAM_INT);
+
+    $query->execute();
+}
+
+function atualizarDetalhesDoProduto(PDO $conexao, array $detalhes): void
+{
+
+    $sql = "UPDATE detalhes_produto SET
+            peso = :peso,
+            dimensoes = :dimensoes,
+            codigo_barras = :codigo_barras,
+            data_validade = :data_validade
+            WHERE produto_id = :produto_id";
+
+    $query = $conexao->prepare($sql);
+    $query->bindValue(":peso", $detalhes['peso'], is_null($detalhes['peso']) ? PDO::PARAM_NULL : PDO::PARAM_STR);
+    $query->bindValue(":dimensoes", $detalhes['dimensoes'], is_null($detalhes['dimensoes']) ? PDO::PARAM_NULL : PDO::PARAM_STR);
+    $query->bindValue(":codigo_barras", $detalhes['codigo_barras'], is_null($detalhes['codigo_barras']) ? PDO::PARAM_NULL : PDO::PARAM_STR);
+    $query->bindValue(":data_validade", $detalhes['data_validade'], is_null($detalhes['data_validade']) ? PDO::PARAM_NULL : PDO::PARAM_STR);
+    $query->bindValue(":produto_id", $detalhes['produto_id'], PDO::PARAM_INT);
+    $query->execute();
 }
