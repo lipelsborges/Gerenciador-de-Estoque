4b3a5ff27475cad900a86183b2ffc1b707bbeb45
Felipe Borges <lipelsborges@gmail.com>
2026-03-05 11:14:21 -0300

    criando a função buscarEstoquePorIds e atualizarEstoque.

--- src/estoque_crud.php
+++ src/estoque_crud.php
@@ -33,3 +33,47 @@ function inserirEstoque($conexao, int $loja_id, int $produto_id, int $estoque):v
     $query->execute();
 
 }
+
+function buscarEstoquePorIds($conexao, $loja_id, $produto_id): ?array {
+
+    $sql = "SELECT
+                lp.loja_id, 
+                lp.produto_id, 
+                lp.estoque,
+                l.nome 'nome_loja', 
+                pr.nome 'nome_produto'
+            FROM lojas_produtos lp
+            INNER JOIN lojas l on lp.loja_id = l.id
+            INNER JOIN produtos pr on lp.produto_id= pr.id
+            WHERE lp.loja_id = :loja_id AND lp.produto_id = :produto_id";
+
+    $query = $conexao->prepare($sql);
+    $query->bindValue(':loja_id', $loja_id, PDO::PARAM_INT);
+    $query->bindValue(':produto_id', $produto_id, PDO::PARAM_INT);
+    $query->execute();
+    $resultado = $query->fetch(PDO::FETCH_ASSOC);
+    return $resultado ?: null;
+}
+
+function atualizarEstoque($conexao, $loja_id, $produto_id, $estoque): void {
+
+    $sql = "UPDATE lojas_produtos SET estoque = :estoque WHERE loja_id = :loja_id AND produto_id = :produto_id";
+
+    $query = $conexao->prepare($sql);
+    $query->bindValue(':estoque', $estoque, PDO::PARAM_INT);
+    $query->bindValue(':loja_id', $loja_id, PDO::PARAM_INT);
+    $query->bindValue(':produto_id', $produto_id, PDO::PARAM_INT);
+    $query->execute();
+
+}
+
+function excluirEstoque($conexao, $loja_id, $produto_id): void {
+
+    $sql = "DELETE FROM lojas_produtos WHERE loja_id = :loja_id AND produto_id = :produto_id";
+
+    $query = $conexao->prepare($sql);
+    $query->bindValue(':loja_id', $loja_id, PDO::PARAM_INT);
+    $query->bindValue(':produto_id', $produto_id, PDO::PARAM_INT);
+    $query->execute();
+
+}
