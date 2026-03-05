1eb9df09eb8af5ef69f11ab6be92c388314cfd7d
Felipe Borges <lipelsborges@gmail.com>
2026-03-05 10:12:54 -0300

    ajuste

--- estoque/excluir.php
+++ estoque/excluir.php
@@ -14,7 +14,7 @@ exigirLogin();
     <h3 class="text-center"><i class="bi bi-trash3-fill"></i> Excluir produto do estoque da loja</h3>
 
     <div class="alert alert-danger w-50 text-center mx-auto">
-        <p>Tem certeza que deseja excluir este produto <b>Nome do produto</b>?</p>
+        <p>Tem certeza que deseja excluir este produto desta loja?</p>
         <a href="listar.php" class="btn btn-secondary"><i class="bi bi-x-circle"></i> Não</a>
         <a href="" class="btn btn-danger"><i class="bi bi-check-circle"></i> Sim</a>
     </div>
