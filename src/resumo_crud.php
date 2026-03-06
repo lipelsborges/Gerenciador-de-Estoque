566d907a4417b9fc469fb4d822d91de2f3c08162
Felipe Borges <lipelsborges@gmail.com>
2026-03-06 09:25:52 -0300

    Criando a função contarProdutosVencidosOVencendo

--- src/resumo_crud.php
+++ src/resumo_crud.php
@@ -1,29 +1,42 @@
 <?php
 
-function contarProdutos(PDO $conexao): int{
+function contarProdutos(PDO $conexao): int
+{
 
     $sql = "SELECT COUNT(*) FROM produtos";
     return (int)$conexao->query($sql)->fetchColumn();
 }
 
-function contarFornecedor(PDO $conexao): int {
+function contarFornecedor(PDO $conexao): int
+{
     $sql = "SELECT COUNT(*) FROM fornecedores";
     return (int)$conexao->query($sql)->fetchColumn();
 }
 
-function contarLojas(PDO $conexao): int {
+function contarLojas(PDO $conexao): int
+{
     $sql = "SELECT COUNT(*) FROM lojas";
     return (int)$conexao->query($sql)->fetchColumn();
 }
 
-function contarEstoqueBaixo(PDO $conexao): int {
+function contarEstoqueBaixo(PDO $conexao): int
+{
     $sql = "SELECT COUNT(*) FROM lojas_produtos WHERE estoque < 5";
     return (int)$conexao->query($sql)->fetchColumn();
 }
 
-function contarLojasSemRegistroDeEstoque(PDO $conexao): int{;
+function contarLojasSemRegistroDeEstoque(PDO $conexao): int
+{;
     $sql = "SELECT COUNT(*) FROM lojas l
             LEFT JOIN lojas_produtos lp ON l.id = lp.loja_id
             WHERE lp.loja_id IS NULL  ";
     return (int)$conexao->query($sql)->fetchColumn();
+}
+
+function contarProdutosVencidosOVencendo(PDO $conexao): int
+{
+    $sql = "SELECT COUNT(*) FROM detalhes_produto
+            WHERE data_validade IS NOT NULL
+            AND data_validade <= DATE_ADD(CURDATE(), INTERVAL 30 DAY);";
+    return (int) $conexao->query($sql)->fetchColumn();
 }
